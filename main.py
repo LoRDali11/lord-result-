8b0e58f4d123fe9a7e158768202a7c2648405564
LoRDali11 <ail112glal@gmail.com>
2026-06-27 12:39:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-kahaj
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+    page.goto("https://services.aun.edu.eg/results/public/ar/exam-result", wait_until="networkidle")
+
+    print("========== SELECT ==========")
+    for s in page.locator("select").all():
+        print(s)
+
+    print("========== INPUT ==========")
+    for i in page.locator("input").all():
+        print(i)
+
+    input("اضغط Enter للإغلاق...")
+
+    browser.close()
